318382e3f48d44014e8d1dd4d0b811ac8bba1f91
manoj-adhikari <imadhikarimanoj@gmail.com>
2024-09-30 20:13:27 +0530

    add

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -12,7 +12,7 @@ const HomePage = () => {
 
   return (
     <div className="home-page"> {/* Updated class name for styling */}
-      <h1>Welcome to Recipe On Go 🍽️</h1>
+      <h1>Welcome to Recipe On Go ‎𐂐◯𓇋</h1>
       <p>
         Are you ready to discover the best recipes that the culinary world has to offer? 
         Our platform is designed to help you search for delicious meals that will tantalize 
